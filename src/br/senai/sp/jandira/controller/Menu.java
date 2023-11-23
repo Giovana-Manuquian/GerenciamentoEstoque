e9adf3779811981fb13071d803c5936782481892
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-11-23 17:16:03 -0300

    finished project

--- src/br/senai/sp/jandira/controller/Menu.java
+++ src/br/senai/sp/jandira/controller/Menu.java
@@ -26,7 +26,7 @@ public class Menu {
             System.out.println("-------------------------------------------------");
             System.out.println("                       Menu                      ");
             System.out.println("-------------------------------------------------");
-            System.out.println("1 - Product                                      ");
+            System.out.println("1 - Employee                                      ");
             System.out.println("2 - Customer                                     ");
             System.out.println("3 - Exit                                         ");
             System.out.println("-------------------------------------------------");
